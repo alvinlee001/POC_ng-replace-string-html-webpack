c9dbdf0cf67193e7d33f07a41d1d04acb6e6c482
Faris Amali Alis <f-a-a@gmx.com>
2020-04-16 11:57:18 +0800

    made loader working

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,18 +1,21 @@
 const webpack = require("webpack");
 const ReplaceInFileWebpackPlugin = require("replace-in-file-webpack-plugin");
 const StringReplacePlugin = require("string-replace-webpack-plugin");
+const AngularCompilerPlugin = require('@ngtools/webpack');
 
-module.exports = {
-  module: {
-    rules: [
+module.exports = (config) => {
+  config.module.rules.unshift(
       // configure replacements for file patterns
       {
-        test: /main.*\.js$/,
+        test: /\.html$/,
         use: [
+          {
+            loader: "raw-loader"
+          },
           {
             loader: "string-replace-loader",
             options: {
-              search: "<t>(.+?)<\/t>",
+              search: '<t>(.+?)<\/t>',
               replace: function(match, $1, index, input) {
                 var original_value = $1;
                 converted_key = original_value
@@ -27,32 +30,18 @@ module.exports = {
           }
         ]
       }
-    ]
-  },
-  plugins: [
+    )
+  const index = config.plugins.findIndex(p => p instanceof AngularCompilerPlugin.AngularCompilerPlugin);
+  const oldOptions = config.plugins[index]._options;
+  oldOptions.directTemplateLoading = false;
+  config.plugins.splice(index);
+  config.plugins.push(new AngularCompilerPlugin.AngularCompilerPlugin(oldOptions));
+  config.plugins.push(
     new webpack.DefinePlugin({
       STABLE_FEATURE: JSON.stringify(true),
       EXPERIMENTAL_FEATURE: JSON.stringify(false)
-    })
-    // new ReplaceInFileWebpackPlugin([
-    //   {
-    //     test: /main.*\.js$/,
-    //     rules: [
-    //       {
-    //         search: /<t>(.+?)<\/t>/g,
-    //         replace: function(match, $1, index, input) {
-    //           var original_value = $1;
-    //           converted_key = original_value
-    //             .toString()
-    //             .toLowerCase()
-    //             .replace(/[^-a-zA-Z0-9 ]*/gi, "")
-    //             .replace(/[ ]/gi, ".");
-    //           return `<t>${converted_key}</t>`;
-    //         }
-    //       }
-    //     ]
-    //   }
-    // ])
-    // new StringReplacePlugin()
-  ]
+    }),
+  )
+  return config;
+
 };
